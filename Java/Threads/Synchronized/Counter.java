2abd66d5aaf23e6edf71504565fdac10aa68ba62
Arhum <147280814+Arhum049@users.noreply.github.com>
2025-03-17 08:42:59 +0530

    Add files via upload

---
+++ Java/Threads/Synchronized/Counter.java
@@ -0,0 +1,12 @@
+public class Counter {
+
+    private int counter = 0;
+
+    public void incrementCounter() {
+        counter++;
+    }
+
+    public int getCount() {
+        return counter;
+    }
+}
